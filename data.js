9ea6d8f7761585d81414bde006059a1d17da0f0b
Thomas Carli <thomascarli@gmail.com>
2024-07-02 15:31:33 +0200

    include data in frontend

---
+++ data.js
@@ -0,0 +1,357 @@
+export const data = {
+  candidatsNonDesistes: [
+    {
+      desiste: false,
+      circonscription: "ALLIER 2e CIRCO.",
+      parti: "LR",
+      nom: "Romain Lefebvre",
+      score: 19.67,
+      insta: {
+        handle: "romainlefebvre03",
+        contactable: true,
+      },
+      x: {
+        handle: "RomainLefebvre_",
+        post: null,
+      },
+      scoresMeilleurs: [
+        {
+          parti: "RN",
+          score: 34.33,
+        },
+        {
+          parti: "LFI-NFP",
+          score: 24.06,
+        },
+      ],
+    },
+    {
+      desiste: false,
+      circonscription: "ALPES-MARITIMES 1e CIRCO.",
+      parti: "HOR.-ENSEMBLE",
+      nom: "Graig Monetti",
+      photo:
+        "https://pbs.twimg.com/profile_images/1730182411495825408/hSBasUQk_400x400.jpg",
+      score: 22.79,
+      insta: {
+        handle: null,
+        contactable: true,
+      },
+      x: {
+        handle: "MonettiGraig",
+        post: "https://x.com/MonettiGraig/status/1808070182394380742",
+      },
+      scoresMeilleurs: [
+        {
+          parti: "LR-RN",
+          score: 41.04,
+        },
+        {
+          parti: "LFI-NFP",
+          score: 26.62,
+        },
+      ],
+    },
+    {
+      desiste: false,
+      circonscription: "ARDÈCHE 3e CIRCO.",
+      parti: "DIV. DR.",
+      nom: "Fabrice Brun",
+      score: 26.48,
+      insta: {
+        handle: "fabrice_brun",
+        contactable: true,
+      },
+      x: {
+        handle: "FabriceBrun",
+        post: "https://x.com/FabriceBrun/status/1807804861473296581",
+      },
+      scoresMeilleurs: [
+        {
+          parti: "RN",
+          score: 31.95,
+        },
+        {
+          parti: "LFI-NFP",
+          score: 28.31,
+        },
+      ],
+    },
+    {
+      desiste: false,
+      circonscription: "BOUCHES-DU-RHÔNE 14e CIRCO.",
+      parti: "REN.-ENSEMBLE",
+      nom: "Anne-Laurence Petel",
+      score: 28.91,
+      insta: {
+        handle: "annelaurencepetel",
+        contactable: true,
+      },
+      x: {
+        handle: "al_petel",
+        post: "https://x.com/al_petel/status/1807875070947479768",
+      },
+      scoresMeilleurs: [
+        {
+          parti: "LR-RN",
+          score: 31.65,
+        },
+        {
+          parti: "PS-NFP",
+          score: 29.48,
+        },
+      ],
+    },
+    {
+      desiste: true,
+      circonscription: "CÔTES D'ARMOR 4e CIRCO.",
+      parti: "REN.-ENSEMBLE",
+      nom: "Cyril Jobic",
+      score: 30.56,
+      insta: {
+        handle: null,
+        contactable: true,
+      },
+      x: {
+        handle: "CyrilJOBIC",
+        post: "https://x.com/CyrilJOBIC/status/1806792103344906367",
+      },
+      scoresMeilleurs: [
+        {
+          parti: "RN",
+          score: 34.3,
+        },
+        {
+          parti: "LFI-NFP",
+          score: 31.03,
+        },
+      ],
+    },
+    {
+      desiste: false,
+      circonscription: "DRÔME 4e CIRCO.",
+      parti: "LR",
+      nom: "Emmanuelle Anthoine",
+      score: 23.98,
+      insta: {
+        handle: null,
+        contactable: true,
+      },
+      x: {
+        handle: "eanthoine26",
+        post: null,
+      },
+      scoresMeilleurs: [
+        {
+          parti: "RN",
+          score: 38.38,
+        },
+        {
+          parti: "PS-NFP",
+          score: 26.27,
+        },
+      ],
+    },
+    {
+      desiste: false,
+      circonscription: "HÉRAULT 1e CIRCO.",
+      parti: "REN.-ENSEMBLE",
+      nom: "Patricia Miralles",
+      score: 22.54,
+      insta: {
+        handle: null,
+        contactable: true,
+      },
+      x: {
+        handle: "MIRALLESMP",
+        post: "https://x.com/MIRALLESMP/status/1806806857459122464",
+      },
+      scoresMeilleurs: [
+        {
+          parti: "LR-RN",
+          score: 34.11,
+        },
+        {
+          parti: "EELV-NFP",
+          score: 34.04,
+        },
+      ],
+    },
+    {
+      desiste: true,
+      circonscription: "ISÈRE 9e CIRCO.",
+      parti: "MODEM-ENSEMBLE",
+      nom: "Élodie Jacquier-Laforge",
+      score: 27.26,
+      insta: {
+        handle: null,
+        contactable: true,
+      },
+      x: {
+        handle: "elodiejala",
+        post: null,
+      },
+      scoresMeilleurs: [
+        {
+          parti: "RN",
+          score: 34.04,
+        },
+        {
+          parti: "LFI-NFP",
+          score: 27.99,
+        },
+      ],
+    },
+    {
+      desiste: false,
+      circonscription: "OISE 7e CIRCO.",
+      parti: "LR",
+      nom: "Maxime Minot",
+      score: 21.52,
+      insta: {
+        handle: null,
+        contactable: true,
+      },
+      x: {
+        handle: "MaximeMinot",
+        post: "https://x.com/MaximeMinot/status/1806756248919928911",
+      },
+      scoresMeilleurs: [
+        {
+          parti: "RN",
+          score: 40.43,
+        },
+        {
+          parti: "PCF-NFP",
+          score: 27.18,
+        },
+      ],
+    },
+    {
+      desiste: true,
+      circonscription: "PYRÉNÉES-ATLANTIQUES 1e CIRCO.",
+      parti: "DIV. G.",
+      nom: "Jean-Yves Lalanne",
+      score: 19.95,
+      insta: {
+        handle: null,
+        contactable: true,
+      },
+      x: {
+        handle: null,
+        post: null,
+      },
+      scoresMeilleurs: [
+        {
+          parti: "RN",
+          score: 27.96,
+        },
+        {
+          parti: "MODEM-ENSEMBLE",
+          score: 26.82,
+        },
+      ],
+    },
+    {
+      desiste: false,
+      circonscription: "RHÔNE 8e CIRCO.",
+      parti: "MODEM-ENSEMBLE",
+      nom: "Nathalie Serre",
+      score: 21.18,
+      insta: {
+        handle: null,
+        contactable: true,
+      },
+      x: {
+        handle: "NathalieSerre",
+        post: "https://x.com/NathalieSerre/status/1806294030591889758",
+      },
+      scoresMeilleurs: [
+        {
+          parti: "RN",
+          score: 33.46,
+        },
+        {
+          parti: "PS-NFP",
+          score: 22.75,
+        },
+        { parti: "MODEM-ENSEMBLE", score: 22.75 },
+      ],
+    },
+    {
+      desiste: false,
+      circonscription: "ESSONNE 2e CIRCO.",
+      parti: "HOR.-ENSEMBLE",
+      nom: "Naima Sifer",
+      score: 22.09,
+      insta: {
+        handle: null,
+        contactable: true,
+      },
+      x: {
+        handle: "NaimaSifer",
+        post: null,
+      },
+      scoresMeilleurs: [
+        {
+          parti: "RN",
+          score: 40.3,
+        },
+        {
+          parti: "LFI-NFP",
+          score: 26.54,
+        },
+      ],
+    },
+    {
+      desiste: false,
+      circonscription: "VAL-D'OISE 1ère CIRCO.",
+      parti: "REN.-ENSEMBLE",
+      nom: "Émilie Chandler",
+      score: 25.46,
+      insta: {
+        handle: null,
+        contactable: true,
+      },
+      x: {
+        handle: "EmilieChandler",
+        post: null,
+      },
+      scoresMeilleurs: [
+        {
+          parti: "RN",
+          score: 33.65,
+        },
+        {
+          parti: "LFI-NFP",
+          score: 30.8,
+        },
+      ],
+    },
+    {
+      desiste: false,
+      circonscription: "CREUSE 1ère CIRCO.",
+      parti: "DIV. DR.",
+      nom: "Valérie Simonet",
+      score: 22.12,
+      insta: {
+        handle: null,
+        contactable: true,
+      },
+      x: {
+        handle: "simonetv23",
+        post: null,
+      },
+      scoresMeilleurs: [
+        {
+          parti: "LR-RN",
+          score: 33.35,
+        },
+        {
+          parti: "LFI-NFP",
+          score: 23.47,
+        },
+      ],
+    },
+  ],
+};
--- main.js
+++ main.js
@@ -1,40 +1,4 @@
-function fallbackCopyTextToClipboard(text) {
-  var textArea = document.createElement("textarea");
-  textArea.value = text;
-
-  // Avoid scrolling to bottom
-  textArea.style.top = "0";
-  textArea.style.left = "0";
-  textArea.style.position = "fixed";
-
-  document.body.appendChild(textArea);
-  textArea.focus();
-  textArea.select();
-
-  try {
-    var successful = document.execCommand("copy");
-    var msg = successful ? "successful" : "unsuccessful";
-    console.log("Fallback: Copying text command was " + msg);
-  } catch (err) {
-    console.error("Fallback: Oops, unable to copy", err);
-  }
-
-  document.body.removeChild(textArea);
-}
-function copyTextToClipboard(text) {
-  if (!navigator.clipboard) {
-    fallbackCopyTextToClipboard(text);
-    return;
-  }
-  navigator.clipboard.writeText(text).then(
-    function () {
-      console.log("Async: Copying to clipboard was successful!");
-    },
-    function (err) {
-      console.error("Async: Could not copy text: ", err);
-    }
-  );
-}
+import { data } from "./data.js";
 
 const messages = [
   "Vous n’avez aucune chance d’emporter le second tour, désistez-vous maintenant ! desiste.fr",
@@ -69,63 +33,59 @@ function getBadgeColor(parti) {
   return colors[parti] || "black";
 }
 
-fetch("https://legislatives.fly.dev/")
-  .then((response) => response.json())
-  .then((data) => {
-    const candidates = data.candidatsNonDesistes;
-    candidates.sort((a, b) => {
-      // first the non desiste
-      // then by higher score
+const candidates = data.candidatsNonDesistes;
+candidates.sort((a, b) => {
+  // first the non desiste
+  // then by higher score
 
-      if (a.desiste && !b.desiste) {
-        return 1;
-      }
+  if (a.desiste && !b.desiste) {
+    return 1;
+  }
 
-      if (!a.desiste && b.desiste) {
-        return -1;
-      }
+  if (!a.desiste && b.desiste) {
+    return -1;
+  }
 
-      return b.score - a.score;
-    });
-    const count = data.candidatsNonDesistes.filter((c) => !c.desiste).length;
-    console.log(candidates);
+  return b.score - a.score;
+});
+const count = data.candidatsNonDesistes.filter((c) => !c.desiste).length;
+console.log(candidates);
 
-    // all elements with class "number" will be updated with the count
-    document.querySelectorAll(".number").forEach((el) => {
-      el.textContent = count;
-    });
+// all elements with class "number" will be updated with the count
+document.querySelectorAll(".number").forEach((el) => {
+  el.textContent = count;
+});
 
-    // we fill the table#candidates with the data
-    const liste = document.querySelector("#candidates");
-    candidates.forEach((candidate, i) => {
-      if (i > 0) {
-        const hr = document.createElement("hr");
-        liste.appendChild(hr);
-      }
-      const div = document.createElement("a");
-      const scoreRN = candidate.scoresMeilleurs.find(
-        (s) => s.parti === "LR-RN" || s.parti === "RN"
-      ).score;
-      console.log(scoreRN);
-      div.classList.add("candidate");
-      if (candidate.desiste) {
-        div.classList.add("desiste");
-      }
-      div.href = getTwitterURL(candidate.x?.handle);
-      div.innerHTML = `
+// we fill the table#candidates with the data
+const liste = document.querySelector("#candidates");
+candidates.forEach((candidate, i) => {
+  if (i > 0) {
+    const hr = document.createElement("hr");
+    liste.appendChild(hr);
+  }
+  const div = document.createElement("a");
+  const scoreRN = candidate.scoresMeilleurs.find(
+    (s) => s.parti === "LR-RN" || s.parti === "RN"
+  ).score;
+  console.log(scoreRN);
+  div.classList.add("candidate");
+  if (candidate.desiste) {
+    div.classList.add("desiste");
+  }
+  div.href = getTwitterURL(candidate.x?.handle);
+  div.innerHTML = `
         <div class="left">
           <div class="name"><b>${
             candidate.nom
           }</b> <div class="badge" style="background-color: ${getBadgeColor(
-        candidate.parti
-      )}">${candidate.parti}</div></div>
+    candidate.parti
+  )}">${candidate.parti}</div></div>
           <span>${candidate.circonscription} • ${
-        candidate.score
-      }% (vs RN à ${scoreRN}%)</span>
+    candidate.score
+  }% (vs RN à ${scoreRN}%)</span>
         </div>
         <div class="right">
         </div>
       `;
-      liste.appendChild(div);
-    });
-  });
+  liste.appendChild(div);
+});
