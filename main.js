f8042a151a457c245b2a6b5f7173ff8c75409074
Basile Bruneau <basile@bruneau.email>
2024-07-02 13:10:59 +0200

    feat: update number

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 fetch("https://legislatives.fly.dev/")
   .then((response) => response.json())
   .then((data) => {
-    console.log(data);
+    const candidates = data.candidatsNonDesistes;
+    const count = candidates.length;
+
+    // all elements with class "number" will be updated with the count
+    document.querySelectorAll(".number").forEach((el) => {
+      el.textContent = count;
+    });
   });
