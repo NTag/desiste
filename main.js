6f4ec51e3ec9b280828efb6b81c065b8d9e27d3a
Basile Bruneau <basile@bruneau.email>
2024-07-02 13:35:56 +0200

    feat: add instagram

--- main.js
+++ main.js
@@ -11,12 +11,23 @@ fetch("https://legislatives.fly.dev/")
     });
 
     // we fill the table#candidates with the data
-    const table = document.querySelector("#candidates");
-    candidates.forEach((candidate) => {
-      const tr = document.createElement("tr");
-      tr.innerHTML = `
-        <td>${candidate.nom}</td>
+    const liste = document.querySelector("#candidates");
+    candidates.forEach((candidate, i) => {
+      if (i > 0) {
+        const hr = document.createElement("hr");
+        liste.appendChild(hr);
+      }
+      const div = document.createElement("div");
+      div.classList.add("candidate");
+      div.innerHTML = `
+        <div class="name">${candidate.nom}</div>
+        <div class="socials">
+          ${
+            candidate.insta &&
+            `<a href="https://instagram.com/${candidate.insta}" target="_blank"><img src="/instagram-logo.svg" /></a>`
+          }
+        </div>
       `;
-      table.appendChild(tr);
+      liste.appendChild(div);
     });
   });
